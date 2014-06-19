544bf9591b5c244d3d2973fc081cd7b9c14aa053
Dan States <unacoder@gmail.com>
2014-06-18 19:31:38 -0700

    adding data.R

---
+++ data.R
@@ -0,0 +1,52 @@
+fetchData <- function (url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip") {
+    file = basename(URLdecode(url))
+    if (!file.exists(file)) {
+        message(paste("Downloading",file,"..."))
+        download.file(url, destfile=file, method = "curl")
+    }
+    # use globally scoped variable to cache zip file name
+    dataZipFile <<- file
+}
+
+getColNames <- function(file = "UCI HAR Dataset/features.txt", force = FALSE) {
+    if (!exists("dataZipFile"))
+        fetchData()
+    # use globally scoped variable to cache names
+    if (!exists("featuresColNames") | force)
+        featuresColNames <<- read.table(
+            unz(dataZipFile, filename = file),
+            header = FALSE, row.names = 1, stringsAsFactors = FALSE,
+            col.names = c("idx", "names")
+        )
+    # strip parenthesis from names?
+    colnames <- gsub("[()-]", "", featuresColNames$names)
+    invisible(colnames)
+}
+
+getActivityLabels <- function(file = "UCI HAR Dataset/activity_labels.txt", 
+                              force = FALSE) {
+    if (!exists("DataZipFile"))
+        fetchData()
+    # use globally scoped variable to cache labels
+    if (!exists("activityLabels") | force)
+        activityLabels <<- read.table(
+            unz(dataZipFile, filename=file),
+            header = FALSE, row.names = 1, stringsAsFactors = TRUE,
+            col.names = c("idx", "activity")
+        )
+    # any processing needed?
+    invisible(activityLabels)
+}
+
+loadTestData <- function(file = "UCI HAR Dataset/test/X_test.txt") {
+    testdata <- read.table(file, header = FALSE,
+                           colClasses = rep("numeric", length(getColNames())),
+                           col.names = getColNames(), nrow = 5
+                           )
+    invisible(testdata)
+}
+
+loadTestDataActivities <- function (file = "UCI HAR Dataset/test/y_test.txt") {
+    testactivities <- read.table(file, header=FALSE, colClasses = c("integer"))
+    invisible(testactivities)
+}
